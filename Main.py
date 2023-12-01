6e84478b41c0038260888cbaa3008dd861454bb3
worladev <confidenceworlasi@gmail.com>
2023-12-01 17:09:42 +0000

    implementation to get published date of articles

--- Main.py
+++ Main.py
@@ -1,11 +1,11 @@
 # import logging
+import requests
+
 from LoggingConfig import LoggingConfig
 from MediaOutletConfigReader import MediaOutletConfigReader
 from WebScraper import WebScraper
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap5
-# from flask_sqlalchemy import SQLAlchemy
-# from flask import Blueprint
 
 
 # Set up logging
@@ -26,6 +26,7 @@ media_list = reader.read()
 scraper = WebScraper(media_list)
 headlines = scraper.crawl_headlines()
 
+
 # def print_headlines(arg):
 #     for headline in headlines:
 #         print(f"Source: {headline.source}, \nHeadline: {headline.headline}, "
@@ -41,11 +42,19 @@ bootstrap = Bootstrap5(app)
 
 
 @app.route('/')
-def index(page=1):
-    # per_page = 10
+def index():
+    page = request.args.get('page', 1, type=int)
+    per_page = 10
+    start_index = (page - 1) * per_page
+    end_index = start_index + per_page
+    page_items = headlines[start_index:end_index]
+    total_pages = len(headlines) // per_page + (len(headlines) // per_page > 0)
+
     # posts = headlines.query.order_by(headlines.time.desc()).paginate(page, per_page, error_out=False)
     return render_template('index.html',
-                           news_headlines=headlines,
+                           news_headlines=page_items,
+                           page=page,
+                           total_pages=total_pages,
                            )
 
 
--- WebScraper.py
+++ WebScraper.py
@@ -2,6 +2,9 @@ import logging
 from bs4 import BeautifulSoup
 from NewsArticle import NewsArticle
 import requests
+
+from dateutil import parser as date_parser
+from dateutil.parser import ParserError
 from datetime import datetime
 import re
 
@@ -19,6 +22,13 @@ class WebScraper:
     def get_media(self):
         return self.media_list
 
+    # def date_formats(self, date_format):
+    #     date_formats = ["%d %B, %Y", "%d-%b-%Y", "%B %d, %Y", "%b-%d-%Y"]
+    #     for date_format in date_formats:
+    #         if format == date_format:
+    #             return date_format
+    #     return datetime.now().date()
+
     # Method to obtain headlines from news platforms based on various urls in a config file
     # It uses the request module to gain permission to scrap data from news platforms
     # It uses Beautiful soup to get all the headlines from the news outlet
@@ -30,55 +40,71 @@ class WebScraper:
         for media_object in self.media_list:
             try:
                 response = requests.get(media_object.url)
+                # regex
+                # date_pattern = re.compile(
+                    # r'(\d{2})[-/]?(\d{2})[-/]?(\d{4})|(\d{1,2})(?:st|nd|rd|th)? (\w+),? (\d{4})')
+
+                # date_pattern = re.compile(
+                    # r'(\d{2})[-/]?(\d{2})[-/]?(\d{4})|(\d{1,2})(?:st|nd|rd|th)? (\w+),? (\d{4}),(\d{1,2})(\w+),(\d{4})')
+
+                date_pattern = re.compile(
+                    r'(\d{2})[-/]?(\d{2})[-/]?(\d{4})|'
+                    r'(\d{1,2})(?:st|nd|rd|th)? (\w+),? (\d{4})|'
+                    r'(\d{1,2})(\w+), (\d{4})|'
+                    r'(\d{2})-(\w{3})-(\d{4})|'
+                    r'(\w+)(\d{1,2}), (\d{4})|'
+                    r'(\w{3})-(\d{1,2})-(\d{4})'
+                )
+
                 # Check for successful response
                 if response.status_code == 200:
                     logging.info(f"Successfully connected to {media_object.url}")
                     soup = BeautifulSoup(response.content, 'html.parser')
                     news_items = soup.find_all('a', href=True)
-                    # log
 
+                    # log
                     for news_item in news_items:
                         url = news_item['href']
                         headline_text = news_item.text.strip()
+
+                        # date_items = soup.find_all('span', class_='date')
+                        date_match = date_pattern.search(headline_text)  # date-code
+                        # date_match = date_pattern.search(url)
+
+                        if date_match:
+                            # date = date_items.text.strip()
+                            year = date_match.group(3) or date_match.group(6)
+                            month = date_match.group(1) or date_match.group(4)
+                            day = date_match.group(2) or date_match.group(5)
+
+                            try:
+                                date = date_parser.parse(f'{month}-{day}-{year}').date()
+                            except ParserError:
+                                date = "Invalid Format"
+                        else:
+                            # If no date match, use current date
+                            date = datetime.now().date()
+                            # date = "##### - Could not pick"
+
                         if url == "" or "video" in url or len(headline_text) < num_of_headline_text:
                             # log
                             continue
                         else:
-                            # Extracting dates using regex
-                            date_pattern = re.compile(
-                                r'(\d{4})[-/]?(\d{2})[-/]?(\d{2})|(\d{1,2})(?:st|nd|rd|th)? (\w+),? (\d{4})')
-                            date_matches = date_pattern.findall(headline_text)
-
-                            # Choose the first match if available
-                            date_match = date_matches[0] if date_matches else None
-
-                            # Extract date components
-                            if date_match:
-                                year = date_match[0] or date_match[5]
-                                month = date_match[1] or date_match[4]
-                                day = date_match[2] or date_match[3]
-                                datetime.strptime(f'{year}-{month}-{day}', '%Y-%m-%d')
-                            else:
-                                # If no date match, use current date
-                                datetime.now()
-
-                            current_date = datetime.now().strftime("%Y-%m-%d")
-
                             article = NewsArticle(
                                 source=media_object.name,
-                                date=current_date,
+                                date=date,
                                 headline=headline_text,
                                 category="some_category",
                                 url=url
                             )
                             # log
                             all_headlines.append(article)
-                    # log
+                        # log
                 else:
                     logging.warning(
                         f"Failed to fetch data from {media_object.url}. Status code: {response.status_code}")
             except requests.RequestException as exception_error:
                 logging.error(f"Error connecting to {media_object.url}: {exception_error}")
         return all_headlines
-    # a method that takes an integer. could use enums
-    # returns meaning of status code received
+        # a method that takes an integer. could use enums
+        # returns meaning of status code received
