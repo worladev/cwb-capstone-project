895b6e95c07b8f22a92583c9336dc26f70bf7695
Daakyehen <enochboakye376@gmail.com>
2023-12-06 06:40:56 +0000

    Added Regex code to the WebScraper.py to get the time used in various mediaOutlets

--- WebScraper.py
+++ WebScraper.py
@@ -24,28 +24,20 @@ class WebScraper:
     def get_media(self):
         return self.media_list
 
-    # def date_formats(self, date_format):
-    #     date_formats = ["%d %B, %Y", "%d-%b-%Y", "%B %d, %Y", "%b-%d-%Y"]
-    #     for date_format in date_formats:
-    #         if format == date_format:
-    #             return date_format
-    #     return datetime.now().date()
 
     # Method to obtain headlines from news platforms based on various urls in a config file
     # It uses the request module to gain permission to scrap data from news platforms
     # It uses Beautiful soup to get all the headlines from the news outlet
 
+
     def crawl_headlines(self, num_of_headline_text=30):
+
         all_headlines = []
         self.logger.info("Scraping headlines from news platforms.")
 
         for media_object in self.media_list:
             try:
                 response = requests.get(media_object.url)
-                # regex
-                # date_pattern = re.compile(
-                #     r'(\d{2})[-/]?(\d{2})[-/]?(\d{4})|(\d{1,2})(?:st|nd|rd|th)? (\w+),? (\d{4})')
-
                 date_pattern = re.compile(
                     r'(\d{2})[-/]?(\d{2})[-/]?(\d{4})|'
                     r'(\d{1,2})(?:st|nd|rd|th)? (\w+),? (\d{4})|'
@@ -67,13 +59,9 @@ class WebScraper:
                         logging.info(f"Obtaining headlines from {media_object.url}")
                         url = news_item['href']
                         headline_text = news_item.text.strip()
-
-                        # date_items = soup.find_all('span', class_='date')
                         date_match = date_pattern.search(headline_text)  # date-code
-                        # date_match = date_pattern.search(url)
 
                         if date_match:
-                            # date = date_items.text.strip()
                             year = date_match.group(3) or date_match.group(6)
                             month = date_match.group(1) or date_match.group(4)
                             day = date_match.group(2) or date_match.group(5)
@@ -85,11 +73,8 @@ class WebScraper:
                         else:
                             # If no date match, use current date
                             date = datetime.now().date()
-                            # date = "##### - Could not pick"
 
                         if url == "" or "video" in url or len(headline_text) < num_of_headline_text:
-                            # log
-
                             logging.info(f"A media item that isn't a headline has been removed.")
 
                             continue
@@ -116,5 +101,3 @@ class WebScraper:
                 logging.error(f"Error connecting to {media_object.url}: {exception_error}")
 
         return all_headlines
-        # a method that takes an integer. could use enums
-        # returns meaning of status code received
