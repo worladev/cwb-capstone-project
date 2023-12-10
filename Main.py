be679a73fe79cd69f8056c1a9a267b6a9af25fbe
worladev <confidenceworlasi@gmail.com>
2023-12-10 16:15:41 +0000

    adding category filters

--- Main.py
+++ Main.py
@@ -21,15 +21,15 @@ logging_config = LoggingConfig()
 # Call the read method to parse the file and get a list of media objects
 # Print the names, URLs, and locations of the media items
 
-# filename = 'config.ini'
-# reader = MediaOutletConfigReader(filename)
-# media_list = reader.read()
-# #
-# #
-# # # # IMPLEMENTING THE WEBSCRAPER CLASS
-# # # # Initialize WebScraper with media objects
-# scraper = WebScraper(media_list)
-# headlines = scraper.crawl_headlines()
+filename = 'config.ini'
+reader = MediaOutletConfigReader(filename)
+media_list = reader.read()
+#
+#
+# IMPLEMENTING THE WEBSCRAPER CLASS
+# Initialize WebScraper with media objects
+scraper = WebScraper(media_list)
+headlines = scraper.crawl_headlines()
 
 
 # def print_headlines(arg):
@@ -38,8 +38,8 @@ logging_config = LoggingConfig()
 #               f"\nDate: {headline.date}, \nLink: {headline.url},\nCategory: {headline.category}\n")
 #
 
-# def print_headlines(headlines):
-#     for category, articles in headlines.items():
+# def print_headlines(headlines_dict):
+#     for category, articles in headlines_dict.items():
 #         print(f"Category: {category}")
 #         for article in articles:
 #             print(f"Source: {article.source}")
@@ -48,8 +48,12 @@ logging_config = LoggingConfig()
 #             print(f"Link: {article.url}")
 #             print(f"Category: {article.category}")
 #             print()
-
+#
+#
 # print_headlines(headlines)
+
+
+filter1 = NewsFilter()
 
 
 # IMPLEMENTING FLASK
@@ -60,29 +64,43 @@ bootstrap = Bootstrap5(app)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     # Reading from config.ini file
-    filename = 'config.ini'
-    reader = MediaOutletConfigReader(filename)
-    media_list = reader.read()
+    # filename = 'config.ini'
+    # reader = MediaOutletConfigReader(filename)
+    # media_list = reader.read()
 
     # IMPLEMENTING THE WEBSCRAPER CLASS
     # Initialize WebScraper with media objects
-    scraper = WebScraper(media_list)
-    headlines = scraper.crawl_headlines()
+    # scraper = WebScraper(media_list)
+    # headlines = scraper.crawl_headlines()
 
     # Paginating and rendering Flask object
-    page = request.args.get('page', 1, type=int)
-    per_page = 10
-    start_index = (page - 1) * per_page
-    end_index = start_index + per_page
-    page_items = headlines[start_index:end_index]
-    total_pages = len(headlines) // per_page + (len(headlines) // per_page > 0)
+    # page = request.args.get('page', 1, type=int)
+    # per_page = 10
+    # start_index = (page - 1) * per_page
+    # end_index = start_index + per_page
+    # page_items = headlines[start_index:end_index]
+    # total_pages = len(headlines) // per_page + (len(headlines) // per_page > 0)
 
     return render_template('index.html',
-                           news_headlines=page_items,
-                           page=page,
-                           total_pages=total_pages,
+                           # news_headlines=page_items,
+                           news_headlines=headlines
+                           # page=page,
+                           # total_pages=total_pages,
                            )
-#
-#
+
+
+@app.route('/category', methods=['GET', 'POST'])
+def news_category():
+    selected_categories = list()
+    if request.method == 'POST':
+        selected_categories = request.form.getlist('check')
+        selected_categories = [category.lower() for category in selected_categories]
+
+    filtered_headlines = filter1.filter_category(headlines, selected_categories)
+    return render_template('category.html',
+                           filtered_headlines=filtered_headlines,
+                           )
+
+
 if __name__ == '__main__':
     app.run(debug=True)
--- NewsFilter.py
+++ NewsFilter.py
@@ -1,10 +1,24 @@
+# class NewsFilter:
+#     @staticmethod
+#     def filter_headlines(headlines_dict, categories):
+#         filtered_headlines = []
+#
+#         for category, headline in headlines_dict.items():
+#             if headline.category.lower() in categories:
+#                 filtered_headlines.append(headline)
+#
+#         return filtered_headlines
+
+
 class NewsFilter:
-    @staticmethod
-    def filter_headlines(headlines, categories):
-        filtered_headlines = []
+    def __init__(self):
+        self.category_headlines = list()
 
-        for headline in headlines:
-            if headline.category.lower() in categories:
-                filtered_headlines.append(headline)
-
-        return filtered_headlines
+    def filter_category(self, headlines_dict, headline_category):
+        for category, headline in headlines_dict.items():
+            category = category.lower()
+            if category in headline_category and category in self.category_headlines:
+                self.category_headlines.append(headline)
+            else:
+                continue
+        return self.category_headlines
